879ec3410f5e9eb5f3f066300c39660219cbf9cf
Igor <cramur@mail.ru>
2011-07-02 19:48:09 +0400

    Classify

--- screenlog.py
+++ screenlog.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # coding: utf-8
 
+
+"""
+Thin timesink clone.
+
+TODO
+====
+screenshot resize
+store path to screenshot in db
+analyze
+pythonize (remove shell calls, use py-xlib)
+
+sqlite3 db creation:
+sqlite logger.db
+create table progs(text, when INTEGER);
+
+"""
+
+
 import sqlite3
 import os
 import sys
@@ -8,41 +26,79 @@ import shlex
 import subprocess
 from datetime import datetime as dt
 from time import time
-#from Xlib.protocol.request import GetInputFocus
 
 
-def MakeName(frmt):
-    return dt.strftime(dt.now(), frmt)
+class ActionLogger(object):
+    """
+    Wrapper for screenshot and db dump
+    """
+    def __init__(self, dbname="logger.db", prefix="/tmp/idid_"):
+        self.dbname = dbname
+        self.prefix = prefix
+        try:
+            self.con = sqlite3.connect(dbname)
+        except OSError:
+            print >>sys.stderr, "WTF?! '%s' DB not found" % dbname
 
 
-def ScreenShot(filename):
-    cmd_line = "import -window root " + filename + ".png"
-    subprocess.Popen(shlex.split(cmd_line))
+    def proceed(self):
+        ''' Actually perform the dirty stuff we are supposing to do. '''
+
+        timestamp = dt.strftime(dt.now(), '%Y-%m-%d_%H:%M:%S')
+        screenshot_filename = "%s%s" % (self.prefix, timestamp)
+        self.grab_screenshot(screenshot_filename)
+        self.get_active_window()
+
+        try:
+            with self.con as con:
+                con.isolation_level = None
+                con.execute("insert into programs values ('%s', '%s')" \
+                    % (self.window_class, timestamp))
+        except sqlite3.IntegrityError:
+            print >>sys.stderr, "Running way too fast?"
+
+        print "Took a screenshot:\n%s\nActive window %s (WM_CLASS='%s')." % (
+            screenshot_filename,
+            self.window_id,
+            self.window_class,
+        )
 
 
-def get_active_window():
-    p1 = subprocess.Popen(["xdpyinfo"], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["grep", "focus"], stdin=p1.stdout, stdout=subprocess.PIPE)
-    p1.stdout.close()
-    output = p2.communicate()[0]
+    def grab_screenshot(self, filename):
+        ''' Take the screenshot with the name supplied as parameter. '''
 
-    window_id = output.split(',')[0].split()[-1]
-    p3 = subprocess.Popen(["xprop", "-id", window_id], stdout=subprocess.PIPE)
-    p4 = subprocess.Popen(["grep", "WM_CLASS"],
-            stdin=p3.stdout,
+        cmd_line = "import -window root " + filename + ".png"
+        if os.system(cmd_line):
+            print >>sys.stderr, "Failed to take a screenshot"
+            raise Exception
+
+
+    def get_active_window(self):
+        ''' Parse X tools output for WM_CLASS '''
+
+        xdpyinfo = subprocess.Popen(["xdpyinfo"], stdout=subprocess.PIPE)
+        grep_focus = subprocess.Popen(["grep", "focus"], stdin=xdpyinfo.stdout, stdout=subprocess.PIPE)
+        xdpyinfo.stdout.close()
+        output = grep_focus.communicate()[0]
+
+        self.window_id = output.split(',')[0].split()[-1]
+
+        xprop = subprocess.Popen(["xprop", "-id", self.window_id], stdout=subprocess.PIPE)
+        grep_for_class = subprocess.Popen(["grep", "WM_CLASS"],
+            stdin=xprop.stdout,
             stdout=subprocess.PIPE)
-    p3.stdout.close()
-    output = p4.communicate()[0]
+        xprop.stdout.close()
+        output = grep_for_class.communicate()[0]
 
-    return output.split('=')[-1].rstrip()
+        self.window_class = output.split('=')[-1].rstrip()
 
+        return self.window_class
 
 
 if __name__ == "__main__":
-    ScreenShot("/tmp/idid_" + MakeName('%Y-%m-%d_%H:%M:%S'))
-    con = sqlite3.connect("logger.db")
-    con.isolation_level = None
-    cur = con.cursor()
-    cur.execute("insert into programs values ('%s', '%s')" \
-            % (get_active_window(), dt.now()))
+    ''' write test info in default db for ActionLogger '''
+
+    act_logger = ActionLogger()
+    act_logger.proceed()
+
 
